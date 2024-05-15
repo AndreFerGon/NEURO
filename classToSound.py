a46d33fd5a298fa54172bc3139ce41f5b7fe6bc8
diogocerveira <up201907427@up.pt>
2024-05-15 03:19:01 +0100

    workings but off downbeat

--- classToSound.py
+++ classToSound.py
@@ -11,6 +11,7 @@ import threading
 import os
 import queue
 import random
+import time
 
 userInput = queue.Queue()
 
@@ -24,11 +25,39 @@ def getInput():
         else:
             print(f"Invalid command! (0 - 1 - 2 - 3 - 4)\n")
 
-def playSounds(audioDict, delay):
+def playSounds():
 
-    # choose the samplerate to play the sound
-    pygame.mixer.pre_init(48000, -16, 2, 2048)  # 44100 Hz, 16-bit signed, stereo, buffer size 2048
+    # receive class label and outputs in real time
+    # load sound files, each intrument (class) has 4 sound samples
+    # 1 - drums, 2 - bass, 3 - guitar, 4 - vocals
+    # 0 - no sound (mode class)
+
+    # print the rules of playing
+    print("\n\n!!! Choose a label:\n1 - drums,\n2 - bass,\n3 - guitar,\n4 - vocals\n")
+    print("!!! Press 0 to switch between instrument and sample choice modes\n")
+    print("!!! Press Ctrl+C to exit\n\n")
+
+    # sound = pygame.mixer.Sound(r"sounds/_archive/audioBeat.wav")
+    # sound.set_volume(0.3)  # Adjust the volume (0.0 - 1.0)
+    # sound.play(-1)  # -1 means loop indefinitely
+    tempo = 115 # bpm for Heart of Glass
+    delay = int(60000 / tempo)   # ms
+    magnet = 16 # 16th notes per bar
+
+    # dictionary with class labels as keys and imported sound samples as values
+    audioDict = audioDict = {
+    1: {1: [], 2: [], 3: [], 4: []},
+    2: {1: [], 2: [], 3: [], 4: []},
+    3: {1: [], 2: [], 3: [], 4: []},
+    4: {1: [], 2: [], 3: [], 4: []}
+}   
+    
+    pygame.mixer.pre_init(48000, -16, 2, 2048)   # setup mixer to avoid sound lag
     pygame.mixer.init()
+
+    song = "heartOfGlass"
+    loadsong(song, audioDict)
+    
     
      # Set to keep track of currently playing sounds
     playingSounds = {}
@@ -37,8 +66,11 @@ def playSounds(audioDict, delay):
     mode = 0    # 0 - instrument choice mode / 1 - sample choice mode
     instrument = 1  # drums
 
+    start_time = time.time()
+    next_downbeat = start_time
     while True:
         
+
         if not userInput.empty():
             # TO RECEIVE CLASS label FROM MATLAB
             label = int(userInput.get());
@@ -58,6 +90,12 @@ def playSounds(audioDict, delay):
                     audioToPlay = (instrument, label); print("Audio", audioToPlay, '\n')  # tuple (instrument, samplerate)
                 
 
+                # # Wait until the next downbeat to play the sound
+                #     while True:
+                #         if (time.time() >= next_downbeat):
+                #             next_downbeat += (delay / 1000) * magnet
+                #             break
+                #         pass
                 # If the sound for this class is already playing, stop it
                     if audioToPlay in playingSounds:
                         playingSounds[audioToPlay].stop()
@@ -65,26 +103,20 @@ def playSounds(audioDict, delay):
                         del playingSounds[audioToPlay]
                     else:
                 # Otherwise, start playing the sound in a loop
-                
                         soundchoice = random.choice(audioDict[audioToPlay[0]][audioToPlay[1]])
-                        playingSounds[audioToPlay] = pygame.mixer.Sound(soundchoice[0])  # choice[0] is the sound data
-
-                        pygame.time.wait(delay) # align next beat
-
-                        playingSounds[audioToPlay].play(1)  # -1 means loop indefinitely
-
-
+                        playingSounds[audioToPlay] = soundchoice
+    
+                        playingSounds[audioToPlay].play(-1)  # -1 means loop indefinitely
+                    
         # if the label is a mode class, switch between modes
             if label == 0: 
                 mode = switchModes(mode)
-                
-
 
 
 def switchModes(mode):  # switch between instrument and sample choice mode
     
     mode = 1 if mode == 0 else 0
-    print("New mode: ", mode, '\n')
+    print(f"Choose a(n) {'instrument' if mode == 1 else 'sample'}\n")
     return mode
 
 '''
@@ -123,45 +155,30 @@ def loadsong(song, audioDict):
             # stems = {}
 
             # add tuples of sound file paths to the dictionary
-            audioDict[instrument][int(stem[1])].append(sf.read(instrumentPath + stem))
+            audioDict[instrument][int(stem[1])].append(pygame.mixer.Sound(instrumentPath + stem))
     print('\n')
     # print(audioDict)
 
 
-def main(): # receive class label and outputs in real time
-    # load sound files, each intrument (class) has 4 sound samples
-    # 1 - drums, 2 - bass, 3 - guitar, 4 - vocals
-    # 0 - no sound (mode class)
-
-    # dictionary with class labels as keys and imported sound samples as values
-    audioDict = audioDict = {
-    1: {1: [], 2: [], 3: [], 4: []},
-    2: {1: [], 2: [], 3: [], 4: []},
-    3: {1: [], 2: [], 3: [], 4: []},
-    4: {1: [], 2: [], 3: [], 4: []}
-}
-
-
-    song = "heartOfGlass"
-    loadsong(song, audioDict)
+def main():
     
 
-    # sound = pygame.mixer.Sound(r"sounds/_archive/audioBeat.wav")
-    # sound.set_volume(0.3)  # Adjust the volume (0.0 - 1.0)
-    # sound.play(-1)  # -1 means loop indefinitely
-    tempo = 115 # bpm for Heart of Glass
-    delay = int(60000 / tempo)   # ms
-
-    
     # have two threads, one for playing sounds and the other for receiving a real time input from the user
+
+    # Thread to play sounds
+    sound_thread = threading.Thread(target=playSounds)
+    sound_thread.start()
+
+    # sleep using os for 3 seconds to allow the sound thread to start
+    time.sleep(1)
 
     # Thread to get user input
     input_thread = threading.Thread(target=getInput)
     input_thread.start()
 
-    # Thread to play sounds
-    sound_thread = threading.Thread(target=playSounds, args=(audioDict, delay))
-    sound_thread.start()
+    
+
+    
     
 
         
