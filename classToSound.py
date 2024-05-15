89bc1d18e27cd1946c998fbfa88d94784d6b8e4f
diogocerveira <up201907427@up.pt>
2024-05-15 02:03:34 +0100

    working not working (SOUND DESTROYED)

--- classToSound.py
+++ classToSound.py
@@ -1,5 +1,5 @@
-# receive numeric class classLabel (4 classes, 1-4) from matlab signal processing
-# and output a sound based on a modifier 5th class (0)
+# receive numeric class label (4 classes, 1-4) from matlab signal processing
+# and output a sound based on a mode 5th class (0)
 
 # import sys
 import numpy as np
@@ -10,6 +10,7 @@ import pygame
 import threading
 import os
 import queue
+import random
 
 userInput = queue.Queue()
 
@@ -17,7 +18,7 @@ userInput = queue.Queue()
 def getInput():
 
     while True:
-        inputtings = input("Enter a command: ")
+        inputtings = input("\n\n!!! Choose a label: ")
         if inputtings.isdigit():
             userInput.put(int(inputtings))
         else:
@@ -25,31 +26,36 @@ def getInput():
 
 def playSounds(audioDict, delay):
 
+    # choose the samplerate to play the sound
+    pygame.mixer.pre_init(48000, -16, 2, 2048)  # 44100 Hz, 16-bit signed, stereo, buffer size 2048
+    pygame.mixer.init()
+    
      # Set to keep track of currently playing sounds
     playingSounds = {}
 
     # default values
-    modifier = 0    # 0 - instrument choice mode / 1 - sample choice mode
+    mode = 0    # 0 - instrument choice mode / 1 - sample choice mode
     instrument = 1  # drums
 
     while True:
         
         if not userInput.empty():
-            # TO RECEIVE CLASS classLabel FROM MATLAB
-            classLabel = userInput.get(); print("classLabel: ", classLabel)
-            print("classLabel: ", classLabel)
+            # TO RECEIVE CLASS label FROM MATLAB
+            label = int(userInput.get());
+            # print("label: ", label, '\n')
 
-        # if the classLabel is a valid non modifier class
-            if classLabel in audioDict:
+
+        # if the label is a valid non mode class
+            if label in (1, 2, 3, 4):
             
             # change instrument
-                if modifier == 0:
-                    print("\n(Instrument choice mode) ")
-                    instrument = classLabel; print("Instrument", instrument)
+                if mode == 0:
+                    instrument = label;
+                    mode = switchModes(mode)
+
             # change sound
-                elif modifier == 1:
-                    print("\n(Sample choice mode) ")
-                    audioToPlay = chooseSound(classLabel, instrument, modifier); print("Audio", audioToPlay)  # tuple (instrument, samplerate)
+                elif mode == 1:
+                    audioToPlay = (instrument, label); print("Audio", audioToPlay, '\n')  # tuple (instrument, samplerate)
                 
 
                 # If the sound for this class is already playing, stop it
@@ -59,42 +65,40 @@ def playSounds(audioDict, delay):
                         del playingSounds[audioToPlay]
                     else:
                 # Otherwise, start playing the sound in a loop
-                        choice = np.random.choice(audioDict[audioToPlay[0]][audioToPlay[1] - 1])
-                        playingSounds[audioToPlay] = pygame.mixer.Sound(choice[0])
+                
+                        soundchoice = random.choice(audioDict[audioToPlay[0]][audioToPlay[1]])
+                        playingSounds[audioToPlay] = pygame.mixer.Sound(soundchoice[0])  # choice[0] is the sound data
 
                         pygame.time.wait(delay) # align next beat
 
                         playingSounds[audioToPlay].play(1)  # -1 means loop indefinitely
 
 
-        # if the classLabel is a modifier class
-            if classLabel == 0: 
-                modifier = 1 if modifier == 0 else 0
-                print("Modifier: ", modifier)
-
-        user_input = None
+        # if the label is a mode class, switch between modes
+            if label == 0: 
+                mode = switchModes(mode)
+                
 
 
-def chooseMode(classLabel, modifier):  # choose between instrument and sample choice mode
+
+def switchModes(mode):  # switch between instrument and sample choice mode
     
-    if classLabel == 0:
-        modifier = 1 if modifier == 0 else 0    # switch between modes
-    return modifier
+    mode = 1 if mode == 0 else 0
+    print("New mode: ", mode, '\n')
+    return mode
 
-def chooseSound(classLabel, instrument, modifier):   # choose the sound to play
+'''
+def chooseSound(label, instrument, mode):   # choose the sound to play
 
-    if classLabel not in range(0, 5):
-        print("Invalid class classLabel")
-        return
-
-    # if in instrument choice mode, choose it and play the default sample (first one)
-    if modifier == 0:
-        instrument = classLabel
+# if in instrument choice mode, choose it and play the default sample (first one)
+    if mode == 0:
+        instrument = label
         return (instrument, 1)
     
-    # if in sample choice mode, choose the sample to play
-    if modifier == 1:
-        return (instrument, classLabel)
+# if in sample choice mode, choose the sample to play
+    if mode == 1:
+        return (instrument, label)
+'''
 
 '''
 def playMidi(midiToPlay):
@@ -107,29 +111,29 @@ def playMidi(midiToPlay):
 
 def loadsong(song, audioDict):
     
-    print("loading song...\n")
+    print("\nloading song stems...")
 # for each folder of instrument type
     for instrument in [1, 2, 3, 4]:
     # for each sound file in the folder
         instrumentPath = f"sounds/{song}/{instrument}/"
         for stem in os.listdir(instrumentPath):
-            print(stem, flush=True)
+            print(stem, end='\t')
 
             # index = stem[:-4].lstrip(str(instrument)) # remove the instrument name and the .wav extension
             # stems = {}
 
             # add tuples of sound file paths to the dictionary
-            audioDict[instrument][int(stem[0])].append(sf.read(instrumentPath + stem))
-    
+            audioDict[instrument][int(stem[1])].append(sf.read(instrumentPath + stem))
+    print('\n')
     # print(audioDict)
 
 
-def main(): # receive class classLabel and outputs in real time
+def main(): # receive class label and outputs in real time
     # load sound files, each intrument (class) has 4 sound samples
     # 1 - drums, 2 - bass, 3 - guitar, 4 - vocals
-    # 0 - no sound (modifier class)
+    # 0 - no sound (mode class)
 
-    # dictionary with class classLabels as keys and imported sound samples as values
+    # dictionary with class labels as keys and imported sound samples as values
     audioDict = audioDict = {
     1: {1: [], 2: [], 3: [], 4: []},
     2: {1: [], 2: [], 3: [], 4: []},
@@ -140,11 +144,8 @@ def main(): # receive class classLabel and outputs in real time
 
     song = "heartOfGlass"
     loadsong(song, audioDict)
-
-
-    pygame.mixer.init()
     
-    
+
     # sound = pygame.mixer.Sound(r"sounds/_archive/audioBeat.wav")
     # sound.set_volume(0.3)  # Adjust the volume (0.0 - 1.0)
     # sound.play(-1)  # -1 means loop indefinitely
@@ -154,11 +155,11 @@ def main(): # receive class classLabel and outputs in real time
     
     # have two threads, one for playing sounds and the other for receiving a real time input from the user
 
-    # Start a new thread to get user input
+    # Thread to get user input
     input_thread = threading.Thread(target=getInput)
     input_thread.start()
 
-    # Start a new thread to play sounds
+    # Thread to play sounds
     sound_thread = threading.Thread(target=playSounds, args=(audioDict, delay))
     sound_thread.start()
     
